d237aadc2e9d9a8ecbf6e4e14b4094a50a96205b
Andres <andreskpognon@gmail.com>
2026-04-19 21:13:13 +0200

    Graph Class

--- src/main/java/modele/Graph.java
+++ src/main/java/modele/Graph.java
@@ -1,4 +1,23 @@
 package modele;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Graph {
+    private HashMap<Integer, ArrayList<Integer>> chListAdjacence;
+
+    public Graph(HashMap<Integer, ArrayList<Integer>> parListAdjacence) {
+        this.chListAdjacence = parListAdjacence;
+    }
+
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[Graph]\n");
+        for(Integer i : this.chListAdjacence.keySet()) {
+            chListAdjacence.get(i);
+            sb.append(i + " : " + this.chListAdjacence.get(i) + "\n");
+        }
+        return sb.toString();
+    }
 }
