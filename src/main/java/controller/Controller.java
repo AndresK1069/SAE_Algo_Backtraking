89ae0dc897e8d07e5d16b90f7aa6cf36b0f4fa05
Andres <andreskpognon@gmail.com>
2026-04-28 17:09:07 +0200

    add EventHandler to the Controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -2,8 +2,10 @@ package controller;
 
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import modele.Graph;
+import modele.GraphExplorationLogger;
 import modele.GraphParser;
 import modele.registry.AlgorithmRegistry;
 import modele.registry.IPathAlgorithm;
@@ -11,30 +13,52 @@ import vue.VboxRoot;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Controller implements EventHandler {
+    private AlgorithmRegistry chAlgorithmRegistry;
+    private GraphParser chGraphParser;
+    private ComboBox<String>  chAlgorithmComboBox;
+    private ComboBox<Integer> chGraphComboBox;
     private VboxRoot root;
 
-    public Controller(VboxRoot root) throws IOException {
-        this.root = root;
-        this.init();
-    }
-
-    private void init() throws IOException {
-        AlgorithmRegistry registry = new AlgorithmRegistry();
-        GraphParser graphParser = new GraphParser();
-        graphParser.preloadGraph("data/");
-
-        ComboBox<String> comboAlgo = (ComboBox<String>) root.lookup("#listAlgorithm");
-        ComboBox<Integer> comboGraph = (ComboBox<Integer>) root.lookup("#listGraphDisponible");
-
-        this.addAlgo(registry,comboAlgo);
-        this.addGraphDisponible(graphParser,comboGraph);
-    }
+    public Controller() {}
 
     @Override
     public void handle(Event event) {
 
+        if (event.getSource() instanceof Button) {
+            Button btn = (Button) event.getSource();
+            switch (btn.getId()) {
+                case "btnSubmit":
+                    this.submitAction();
+            }
+        }
+
+    }
+
+    private void init() throws IOException {
+        chAlgorithmRegistry = new AlgorithmRegistry();
+        chGraphParser = new GraphParser();
+        chGraphParser.preloadGraph("data/");
+
+        chAlgorithmComboBox = (ComboBox<String>) root.lookup("#listAlgorithm");
+        chGraphComboBox = (ComboBox<Integer>) root.lookup("#listGraphDisponible");
+
+        this.addAlgo(chAlgorithmRegistry,chAlgorithmComboBox);
+        this.addGraphDisponible(chGraphParser,chGraphComboBox);
+    }
+
+    private void submitAction(){
+        String algoID = chAlgorithmComboBox.getSelectionModel().getSelectedItem();
+        Integer graphNumber = chGraphComboBox.getSelectionModel().getSelectedItem();
+
+        Optional<IPathAlgorithm> algo = chAlgorithmRegistry.getAlgorithmById(algoID);
+        Graph graph = chGraphParser.getGraphs().get(graphNumber);
+
+        GraphExplorationLogger log = algo.get().compute(graph);
+        root.setLogger(log);
+        root.setTextArea(log.toString());
     }
 
     private void addAlgo(AlgorithmRegistry registry , ComboBox<String> comboAlgo) {
@@ -49,4 +73,10 @@ public class Controller implements EventHandler {
             comboGraph.getItems().add(i);
         }
     }
+
+    public void setRoot(VboxRoot root) throws IOException {
+        this.root = root;
+        this.init();
+    }
+
 }
