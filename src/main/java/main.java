062a18b635218cef760cc7ee08b87936abb7bcfd
Andres <andreskpognon@gmail.com>
2026-04-28 17:07:19 +0200

    fixed main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,38 +2,16 @@ import controller.Controller;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import modele.GraphExplorationLogger;
-import modele.GraphParser;
-import modele.registry.AlgorithmRegistry;
-import modele.registry.IPathAlgorithm;
 import vue.VboxRoot;
-
-import java.util.Optional;
 
 public class main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        AlgorithmRegistry registry = new AlgorithmRegistry();
-        GraphParser graphParser = new GraphParser();
-        graphParser.preloadGraph("data/");
-        Optional<IPathAlgorithm> algo = registry.getAlgorithmById("backtracking");
-
-        GraphExplorationLogger log = algo.get().compute(graphParser.getGraphs().get(5));
-
-        System.out.println("Path:         " + log.getChHamiltonienPath());
-        System.out.println("Path size:    " + log.getChHamiltonienPath().size());
-        System.out.println("Visited:      " + log.getChVisitedNodes());
-        System.out.println("Visited size: " + log.getChVisitedNodes().size());
-        System.out.println("Time (ms):    " + log.getChExplorationTime());
-        System.out.println(log.getChGraph());
-
-
-        //TODO REMOVE ABOVE
-
-        VboxRoot root = new VboxRoot(log);
-        Controller controller = new Controller(root);
-        Scene scene = new Scene(root, 700, 600);
+        Controller controller = new Controller();
+        VboxRoot root = new VboxRoot(controller::handle);
+        controller.setRoot(root);
+        Scene scene = new Scene(root, 1000, 900);
         stage.setTitle("Graph Visualizer");
         stage.setScene(scene);
         stage.show();
